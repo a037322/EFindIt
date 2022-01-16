b9feea32e6d68ed5b0b6cb75ecda9fd154ae5c77
a037322 <a037322@ismai.pt>
2022-01-16 15:18:18 +0000

    pages layout done
    register login and splash screen

--- app/src/main/java/pt/ismai/inf/ricardosousa/e_findit/HomeActivity.kt
+++ app/src/main/java/pt/ismai/inf/ricardosousa/e_findit/HomeActivity.kt
@@ -2,11 +2,10 @@ package pt.ismai.inf.ricardosousa.e_findit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 
-class SplashActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_home)
     }
 }
--- app/src/main/java/pt/ismai/inf/ricardosousa/e_findit/MainActivity.kt
+++ app/src/main/java/pt/ismai/inf/ricardosousa/e_findit/MainActivity.kt
@@ -1,11 +1,21 @@
 package pt.ismai.inf.ricardosousa.e_findit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }

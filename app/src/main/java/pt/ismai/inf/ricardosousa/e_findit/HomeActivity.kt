c5caabff8317722adff1f0472c3e54ac3625d6f5
a037322 <a037322@ismai.pt>
2022-01-21 00:05:53 +0000

    map fragment working
    also all other fragments in the nav bar loading correclty

--- app/src/main/java/pt/ismai/inf/ricardosousa/e_findit/HomeActivity.kt
+++ app/src/main/java/pt/ismai/inf/ricardosousa/e_findit/HomeActivity.kt
@@ -3,9 +3,12 @@ package pt.ismai.inf.ricardosousa.e_findit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -24,16 +27,21 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         supportActionBar?.hide()
         //replaceFragment(mapFragment)
-
+        /*
         mapFragment = supportFragmentManager.findFragmentById(R.id.fragment_map_container) as SupportMapFragment
         mapFragment.getMapAsync(OnMapReadyCallback {
             googleMap = it
-        })
-
+        })*/
+        val bottomNavigationView = findViewById<BottomNavigationView
+                >(R.id.bottom_navigation_bar)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(navController
+        )
+        /*
         bottom_navigation_bar?.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.nav_map -> {
-                    replaceFragment(mapFragment)
+                    replaceMapFragment(mapFragment)
                 }
                 R.id.nav_veiculo -> {
                     replaceFragment(veiculoFragment)
@@ -48,15 +56,14 @@ class HomeActivity : AppCompatActivity() {
                     replaceFragment(guardadosFragment)
                 }
             }
-            true
+            true*/
         }
     }
-
+    /*
     private fun replaceFragment(fragment: Fragment){
         if (fragment != null){
             val transation = supportFragmentManager.beginTransaction()
             transation.replace(R.id.fragment_container, fragment)
             transation.commit()
         }
-    }
-}
+    }*/
--- app/src/main/java/pt/ismai/inf/ricardosousa/e_findit/MapFragment.kt
+++ app/src/main/java/pt/ismai/inf/ricardosousa/e_findit/MapFragment.kt
@@ -1,81 +1,84 @@
 package pt.ismai.inf.ricardosousa.e_findit
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
-    private lateinit var mMap : GoogleMap
-    private var mapReady = false
-    private lateinit var viewModel: HomeActivity
-    /*
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var rootView = inflater.inflate(R.layout.fragment_map, container, false)
-        var mapFragment = childFragmentManager.findFragmentById(R.id.fragment_container) as SupportMapFragment
-        mapFragment.getMapAsync {
-            googleMap -> mMap = googleMap
-            mapReady = true
-            updateMap()
+        val rootView = inflater.inflate(R.layout.fragment_map, container, false)
+
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.fragment_map_container) as SupportMapFragment?  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment!!.getMapAsync { mMap ->
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+            mMap.clear() //clear old markers
+            /*
+            val googlePlex = CameraPosition.builder()
+                .target(LatLng(37.4219999, -122.0862462))
+                .zoom(10f)
+                .bearing(0f)
+                .tilt(45f)
+                .build()
+
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null)
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(37.4219999, -122.0862462))
+                    .title("Spider Man")
+                    .icon(bitmapDescriptorFromVector(activity, R.drawable.ic_baseline_location_on_24))
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(37.4629101, -122.2449094))
+                    .title("Iron Man")
+                    .snippet("His Talent : Plenty of money")
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(37.3092293, -122.1136845))
+                    .title("Captain America")
+            )
+
+             */
         }
+
         return rootView
-    }*/
+    }
     /*
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(it!!).get(HomeActivity::class.java)
-    }
+    private fun bitmapDescriptorFromVector(context: Context?, vectorResId: Int): BitmapDescriptor {
+        val vectorDrawable = ContextCompat.getDrawable(requireContext(), vectorResId)
+        vectorDrawable!!.setBounds(0, 0, vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight)
+        val bitmap =
+            Bitmap.createBitmap(vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }*/
 
-    private fun updateMap() {
-
-    }
-    */
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MapFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
